c88401df5948ace52098bc2a509e343839546f03
stzups <lemonpickles1558@gmail.com>
2021-07-14 18:15:33 -0400

    add manual look and move

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,6 +1,6 @@
-import {Vec3} from "vec3"
-import {Bot, BotOptions} from "mineflayer"
-import {Block} from "prismarine-block";
+import {Vec3} from 'vec3'
+import {Bot, BotOptions} from 'mineflayer'
+import {Block} from 'prismarine-block';
 
 const mineflayer = require('mineflayer')
 const v = require('vec3')
@@ -45,26 +45,93 @@ bot.on('spawn', async () => {
     bot.on('whisper', async (username: string, message: string) => {
         if (ops.includes(username)) {
             try {
-                switch (message) {
-                    case "mine":
+                let split: string[] = message.split(' ')
+                if (split.length === 0) {
+                    bot.whisper(username, 'No arguments specified')
+                }
+                switch (split[0]) {
+                    case 'mine':
                         bot.whisper(username, 'mining')
-                        mine(bot, new Vec3(0, 0, 0), new Vec3(0, 0, 0), new Vec3(1, 0, 0));
+                        mine(bot, new Vec3(0, 0, 0), new Vec3(0, 0, 0), new Vec3(1, 0, 0))
                         break
-                    case "sleepwake":
+                    case 'sleepwake':
                         await sleepWake(bot, bed)
                         bot.whisper(username, 'slept')
                         break
+                    case 'move':
+                        let move = async (direction: string, ticks: number) => {
+                            bot.setControlState(direction, true)
+                            await bot.waitForTicks(ticks)
+                            bot.setControlState(direction, false)
+                        }
+                        let ticks = 5;
+                        if (split.length <= 1) {
+                            bot.whisper(username, 'Not enough movement arguments')
+                            return;
+                        }
+                        for (let i = 0; i < split[1].length; i++) {
+                            let direction: string;
+                            let raw = split[1].charAt(i)
+                            switch (raw) {
+                                case 'w':
+                                    direction = 'forward'
+                                    break
+                                case 'a':
+                                    direction = 'left'
+                                    break
+                                case 's':
+                                    direction = 'back'
+                                    break
+                                case 'd':
+                                    direction = 'right'
+                                    break
+                                case 'j':
+                                    direction = 'jump'
+                                    break
+                                default:
+                                    bot.whisper(username, 'Unknown direction ' + raw)
+                                    return
+                            }
+                            await move(direction, direction !== 'jump' ? ticks : 0)
+                        }
+                        break
+                    case 'look':
+                        if (split.length !== 4) {
+                            bot.whisper(username, 'Incorrect arguments, try look 0 0 0')
+                            return
+                        }
+
+                        let coords = parseInts(split, 1, 3)
+                        bot.lookAt(bot.entity.position.clone().add(new Vec3(coords[0], coords[1], coords[2])))
+                        break
                     default:
-                        throw new Error("Unknown command " + message)
+                        bot.whisper(username, 'Unknown command ' + message)
                 }
             } catch (e) {
                 if (e instanceof Error) {
-                    bot.whisper(username, e.message)
+                    bot.whisper(username, 'Error: ' + e.message)
                 }
             }
         }
     })
 })
+
+function parseInts(strings: string[], offset: number, amount: number): number[] {
+    let numbers: number[] = [];
+
+    if (strings.length - 1 < amount) {
+        throw new Error('Not enough arguments')
+    }
+
+    for (let i = 0; i < strings.length && (amount <= 0 || i < amount); i++) {
+        numbers[i] = parseInt(strings[i + 1])
+        if (isNaN(numbers[i])) {
+            throw new Error(strings[i + 1] + ' is not a number')
+        }
+    }
+
+    return numbers;
+}
 
 async function sleepWake(bot: Bot, bed: Vec3) {
     try {
@@ -97,11 +164,8 @@ async function mine(bot: Bot, input: Vec3, output: Vec3, add: Vec3) {
     bot.setControlState('forward', true);
     while (true) {
         let pos = bot.entity.position.clone().add(add);
-        if (!bottom) {
-            pos.add(up);
-        }
-        await bot.dig(getBlock(bot, pos), false);
-        await bot.waitForTicks(2);
+        await bot.dig(getBlock(bot, pos));
+        await bot.dig(getBlock(bot, pos.add(up)));
         bottom = !bottom;
     }
 }
