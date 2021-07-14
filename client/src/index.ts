019c8632855736a726c1d5575dbf68f1addd9855
stzups <lemonpickles1558@gmail.com>
2021-07-14 12:35:26 -0400

    fix ts and use commonjs :(

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,12 +1,36 @@
-import { Bot, createBot } from "mineflayer";
+import {Vec3} from "vec3";
+import {BotOptions} from "mineflayer";
 
-const bot: Bot = createBot({
+const mineflayer = require('mineflayer');
+const v = require('vec3');
+const data = require('minecraft-data')('1.16.5');
+
+const botOptions: BotOptions = {
     host: 'localhost',
-    port: 25565,
-    username: 'player',
-    password: 'password'
+    username: 'player'
+}
+
+
+console.log(v);
+const bot = mineflayer.createBot(botOptions);
+
+bot.on('error', console.log);
+
+bot.on('kicked', () => {
+    bot.connect(botOptions);
+})
+
+bot.on('entityHurt', () => {
+    bot.chat('ouch');
 });
 
-console.log(bot);
-bot.chat('hello');
-bot.quit('you suck lol');
+bot.on('death', () => {
+    bot.chat('ahhhhhh');
+})
+
+bot.on('spawn', () => {
+    bot.chat('hello');
+    bot.entity.position
+});
+
+let bed: Vec3 = new Vec3(188, 79, -224);
