5068fc12f17188da5dde3797dfc366f57d1f7174
stzups <lemonpickles1558@gmail.com>
2021-07-18 10:41:23 -0400

    clean up

--- client/src/BotHandler.ts
+++ client/src/BotHandler.ts
@@ -13,43 +13,18 @@ export default class BotHandler {
     bot: Bot
 
     constructor(botOptions: BotOptions) {
-        this.bot = mineflayer.createBot(botOptions)
-
         console.log('Creating bot with username ' + botOptions.username)
-
+        this.bot = mineflayer.createBot(botOptions)
         this.bot.on('error', console.log)
 
-        this.bot.on('login', () => {
-            console.log('Logged in to ' + botOptions.host)
-        })
-        this.bot.on('spawn', () => {
-            console.log('Spawned')
-        })
+        this.bot.on('login', () => console.log('Logged in to ' + botOptions.host))
+        this.bot.on('spawn', () => console.log('Spawned'))
+        this.bot.on('kicked', (reason: String) => console.log('Kicked: ' + reason))
+        this.bot.on('entityHurt', () => console.log('entityHurt'))
+        this.bot.on('death', () => console.log('death'))
 
-        this.bot.on('kicked', (reason: String) => {
-            console.log('Kicked: ' + reason)
-        })
-
-        this.bot.on('entityHurt', () => {
-            console.log('entityHurt')
-        })
-
-        this.bot.on('death', () => {
-            console.log('death')
-        })
-
-
-
-
-
-        this.bot.on('death', () => {
-            this.bot.chat('ahhhhhh')
-        })
-
-        this.bot.on('entityHurt', () => {
-            this.bot.chat('ouch')
-        })
-
+        this.bot.on('death', () => this.bot.chat('ahhhhhh'))
+        this.bot.on('entityHurt', () => this.bot.chat('ouch'))
         this.bot.on('spawn', async () => {
             new CommandHandler(this.bot, ops, new FarmHandler(this.bot, new MoveHandler(this.bot)))
 
